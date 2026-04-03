e175b211f1ff9ee78ca575c12c612ac6fabfc12c
Saima Usman <dzinesfactory@gmail.com>
2026-04-04 02:01:53 +0400

    Changed home route response to HTML format
    Update home route to return HTML content instead of plain text.

--- Task#1/app.py
+++ Task#1/app.py
@@ -3,7 +3,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Dockerized Flask Application!"
+    return """
+    <html>
+        <head>
+            <title>Flask App</title>
+        </head>
+        <body style="display:flex; justify-content:center; align-items:center; height:100vh; margin:0;">
+            <h1>Welcome to Dockerized Flask Application!</h1>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
