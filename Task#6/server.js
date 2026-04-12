6f54f17b721fc3f0c18153d2f7e665698033f2e5
Saima Usman <dzinesfactory@gmail.com>
2026-04-13 01:43:09 +0400

    Created server.js

---
+++ Task#6/server.js
@@ -0,0 +1,13 @@
+// server.js
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("👋 Hello &  Welcome to  HeroVired!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
