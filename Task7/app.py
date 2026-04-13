cc7b89d29e942b863d1f75f39054081608532e40
Saima Usman <dzinesfactory@gmail.com>
2026-04-13 20:32:47 +0400

    Created app.py

--- Task7/app.py
+++ Task7/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1 style='font-size:60px;'>👋 Hello from Flask Multi-Stage Build!</h1>"
+    return "<h3 style='font-size:60px;'>👋 Hello from Flask Multi-Stage Build!</h3>"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
